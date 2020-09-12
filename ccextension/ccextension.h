18e2f7aa5f6dd97b243e96ac0e46c4f7cd3e1e48
Quang Truong <>
2020-09-12 20:31:45 +0700

    Init project

---
+++ ccextension/ccextension.h
@@ -0,0 +1,19 @@
+//
+//  ccextension.h
+//  ccextension
+//
+//  Created by cypcap on 9/12/20.
+//  Copyright © 2020 cypcap. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ccextension.
+FOUNDATION_EXPORT double ccextensionVersionNumber;
+
+//! Project version string for ccextension.
+FOUNDATION_EXPORT const unsigned char ccextensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ccextension/PublicHeader.h>
+
+
---
+++ ccextensionexample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ccextensionexample
+//
+//  Created by cypcap on 9/12/20.
+//  Copyright © 2020 cypcap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
